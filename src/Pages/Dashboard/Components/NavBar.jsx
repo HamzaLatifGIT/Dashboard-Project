70412fc887dc8aa541598502d86cfbcdc5caa448
JunaidLatif4m <junaidlatif004@gmail.com>
2024-04-18 00:27:27 +0500

    Logout Auth Added

--- src/Pages/Dashboard/Components/NavBar.jsx
+++ src/Pages/Dashboard/Components/NavBar.jsx
@@ -28,6 +28,10 @@ const NavBar = ({ toggleDrawer }) => {
     const handleClose = () => {
         setAnchorEl(null);
     };
+    const handleLogOut = () => {
+        localStorage.clear()
+        window.location.href = "/"
+    }
     return (
         <>
             <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "0rem 1rem", width: "100%", minHeight: "80px", height: "80x" }}>
@@ -39,64 +43,60 @@ const NavBar = ({ toggleDrawer }) => {
                 <Box  >
                     <IconButton sx={{ padding: "0", height: "40px", width: "40px", overflow: "hidden", backgroundColor: "#0000000a" }} onClick={handleClick}
                         size="small"
-                        
-
                         aria-controls={open ? 'account-menu' : undefined}
                         aria-haspopup="true"
-                        aria-expanded={open ? 'true' : undefined}> <Box component={"img"} sx={{ width: "80%", height: "80%" }} src={GenrateImage({}, "profile")} alt="" /> <Menu
-                            anchorEl={anchorEl}
-                            id="account-menu"
-                            open={open}
-                            onClose={handleClose}
-                            onClick={handleClose}
+                        aria-expanded={open ? 'true' : undefined}> <Box component={"img"} sx={{ width: "80%", height: "80%" }} src={GenrateImage({}, "profile")} alt="" />
+                    </IconButton>
+                    <Menu
+                        anchorEl={anchorEl}
+                        id="account-menu"
+                        open={open}
+                        onClose={handleClose}
+                        onClick={handleClose}
 
-                            PaperProps={{
-                                elevation: 0,
-                                sx: {
-                                    overflow: 'visible',
-                                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                                    mt: 1.5,
-                                    '& .MuiAvatar-root': {
-                                        width: 32,
-                                        height: 32,
-                                        ml: -0.5,
-                                        mr: 1,
-                                    },
-                                    '&::before': {
-                                        content: '""',
-                                        display: 'block',
-                                        position: 'absolute',
-                                        top: 0,
-                                        right: 14,
-                                        width: 10,
-                                        height: 10,
-                                        bgcolor: 'background.paper',
-                                        transform: 'translateY(-50%) rotate(45deg)',
-                                        zIndex: 0,
-                                    },
+                        PaperProps={{
+                            elevation: 0,
+                            sx: {
+                                overflow: 'visible',
+                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                mt: 1.5,
+                                '& .MuiAvatar-root': {
+                                    width: 32,
+                                    height: 32,
+                                    ml: -0.5,
+                                    mr: 1,
                                 },
-                            }}
-                            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                        >
+                                '&::before': {
+                                    content: '""',
+                                    display: 'block',
+                                    position: 'absolute',
+                                    top: 0,
+                                    right: 14,
+                                    width: 10,
+                                    height: 10,
+                                    bgcolor: 'background.paper',
+                                    transform: 'translateY(-50%) rotate(45deg)',
+                                    zIndex: 0,
+                                },
+                            },
+                        }}
+                        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                    >
+                        <MenuItem onClick={handleClose}>
+                            <ListItemIcon>
+                                <Settings fontSize="small" />
+                            </ListItemIcon>
+                            Settings
+                        </MenuItem>
+                        <MenuItem onClick={handleLogOut}>
+                            <ListItemIcon>
+                                <Logout fontSize="small" />
+                            </ListItemIcon>
+                            Logout
+                        </MenuItem>
+                    </Menu>
 
-                            <MenuItem onClick={handleClose}>
-                                <Avatar /> My account
-                            </MenuItem>
-
-                            <MenuItem onClick={handleClose}>
-                                <ListItemIcon>
-                                    <Settings fontSize="small" />
-                                </ListItemIcon>
-                                Settings
-                            </MenuItem>
-                            <MenuItem onClick={handleClose}>
-                                <ListItemIcon>
-                                    <Logout fontSize="small" />
-                                </ListItemIcon>
-                                Logout
-                            </MenuItem>
-                        </Menu> </IconButton>
                 </Box>
             </Box>
         </>
